855c5053126947b5058a3c3343ccc0d3b54c4124
lyaha <lyah.artem10@mailru>
2021-07-18 16:30:03 +0300

    test commit

--- src/main/java/devanmejia/productshop/controllers/TestRepository.java
+++ src/main/java/devanmejia/productshop/controllers/TestRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface TestRepository extends JpaRepository<StockProduct, Long> {
     @Query(value = "SELECT *\n" +
-            "FROM pg_catalog.pg_tables\n" +
-            "WHERE schemaname != 'pg_catalog' AND\n" +
-            "        schemaname != 'information_schema'", nativeQuery = true)
+            "FROM pg_catalog.pg_tables\n", nativeQuery = true)
     String test();
 }
